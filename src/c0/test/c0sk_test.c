d740241a9aae4a51ac2ca3c1dce0b84f4c558836
Tristan Partin <tpartin@micron.com>
2020-09-10 16:27:59 -0500

    Convert t_sync

--- src/c0/test/c0sk_test.c
+++ src/c0/test/c0sk_test.c
@@ -410,14 +410,16 @@ MTF_DEFINE_UTEST_PREPOST(c0sk_test, ingest_debug, no_fail_pre, no_fail_post)
 
 MTF_DEFINE_UTEST_PREPOST(c0sk_test, t_sync, no_fail_pre, no_fail_post)
 {
-    struct kvdb_rparams kvdb_rp;
-    struct kvs_rparams  kvs_rp;
-    struct c0 *         test_c0 = 0;
-    struct kvs_ktuple   kt = { 0 };
-    merr_t              err;
-    struct mock_kvdb    mkvdb;
-    struct cn *         mock_cn;
-    atomic64_t          seqno;
+    struct kvdb_rparams   kvdb_rp;
+    struct kvs_rparams    kvs_rp;
+    struct c0_kvmultiset *kvms;
+    struct kvs_ktuple     kt = { 0 };
+    merr_t                err;
+    struct mock_kvdb      mkvdb;
+    struct cn *           mock_cn;
+    struct c0sk_impl *    self;
+    atomic64_t            seqno;
+    u16                   skidx = 0;
 
     kvdb_rp = kvdb_rparams_defaults();
     kvs_rp = kvs_rparams_defaults();
@@ -432,25 +434,31 @@ MTF_DEFINE_UTEST_PREPOST(c0sk_test, t_sync, no_fail_pre, no_fail_post)
     err = create_mock_cn(&mock_cn, false, false, &kvs_rp, 0);
     ASSERT_EQ(0, err);
 
-    err = c0_open((struct ikvdb *)&mkvdb, &kvs_rp, mock_cn, 0, &test_c0);
-    ASSERT_EQ(0, err);
-    ASSERT_NE(0, test_c0);
-
-    kt.kt_len = 3;
-    kt.kt_data = "foo";
-
-    err = c0_del(test_c0, &kt, HSE_SQNREF_SINGLE);
+    err = c0sk_c0_register(mkvdb.ikdb_c0sk, mock_cn, &skidx);
     ASSERT_EQ(0, err);
 
-    err = c0_sync(test_c0);
+    self = c0sk_h2r(mkvdb.ikdb_c0sk);
+
+    err = c0kvms_create(1, 0, 0, &seqno, false, &kvms);
+    ASSERT_EQ(0, err);
+    ASSERT_NE(NULL, kvms);
+
+    err = c0sk_install_c0kvms(self, NULL, kvms);
+    ASSERT_EQ(0, err);
+
+    kvs_ktuple_init(&kt, "foo", 3);
+
+    err = c0sk_del(mkvdb.ikdb_c0sk, skidx, &kt, HSE_SQNREF_SINGLE);
+    ASSERT_EQ(0, err);
+
+    err = c0sk_sync(mkvdb.ikdb_c0sk);
     ASSERT_EQ(0, err);
 
     /* [HSE_REVISIT] Replace with cndb_ingest.
      ASSERT_EQ(1, mapi_calls(mapi_idx_cn_ingestv));
     */
 
-    err = c0_close(test_c0);
-    ASSERT_EQ(0, err);
+    c0kvms_putref(kvms);
 
     err = c0sk_close(mkvdb.ikdb_c0sk);
     ASSERT_EQ(0, err);
