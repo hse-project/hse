6d9628b14095cfec33865b7e09f02b19bf34d538
David Boles <dboles@micron.com>
2020-10-06 10:38:34 -0500

    Removed post install dt.log setup

--- scripts/rpm/rpm-post-install.sh
+++ scripts/rpm/rpm-post-install.sh
@@ -5,37 +5,7 @@
 # add jni lib to cache
 /sbin/ldconfig
 
-# create dt.log
-
-PERMS=666
-MAXSIZE=10M # When to rotate logs
-MINSIZE=9M  # Until when not to rotate logs
-ARCHIVE_CNT=100 # Max compressed archive files to maintain at any point in time
-
-conf="# Config to rotate dt.log
-/var/log/hse/dt.log{
-    create $PERMS
-    compress
-    notifempty
-    rotate $ARCHIVE_CNT
-    nomissingok
-    weekly
-    size $MAXSIZE
-    minsize $MINSIZE
-    maxage 90
-}
-"
-
-conf_hse="/etc/logrotate.d/hse"
-dtlog="/var/log/hse/dt.log"
-
-# logrotate config
-rm -f $conf_hse
-printf "$conf" > $conf_hse
-
 # log file
 mkdir -p /var/log/hse
-touch $dtlog
-chmod $PERMS $dtlog
 
 exit 0
