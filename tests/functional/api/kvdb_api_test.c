4a2d1e4e98538fa1c0b254c55dbf157e25c31fde
LEROY JACOB VARGIS <ljacobvargis@micron.com>
2021-03-18 12:57:18 -0500

    remove unused headers

--- tests/functional/api/kvdb_api_test.c
+++ tests/functional/api/kvdb_api_test.c
@@ -3,11 +3,7 @@
  * Copyright (C) 2015-2020 Micron Technology, Inc.  All rights reserved.
  */
 
-#include <hse_util/platform.h>
-#include <hse_util/compression_lz4.h>
-
 #include <hse_ut/framework.h>
-
 #include <errno.h>
 
 /* Globals */
