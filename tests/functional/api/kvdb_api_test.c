f387d7cbb4cf868aeaeb8f462d66be6f2d9319d2
LEROY JACOB VARGIS <ljacobvargis@micron.com>
2021-03-18 16:47:07 -0500

    reorganize test cases

--- tests/functional/api/kvdb_api_test.c
+++ tests/functional/api/kvdb_api_test.c
@@ -7,12 +7,12 @@
 #include <errno.h>
 
 /* Globals */
-char *           MPOOL_NAME;
-struct hse_kvdb *KVDB_HANDLE = NULL;
+char *mpool_name;
 
 int
 test_collection_setup(struct mtf_test_info *info)
 {
+    hse_err_t                  rc;
     struct mtf_test_coll_info *coll_info = info->ti_coll;
     hse_openlog("kvdb_api_test", 1);
 
@@ -21,9 +21,11 @@ test_collection_setup(struct mtf_test_info *info)
         return -1;
     }
 
-    MPOOL_NAME = coll_info->tci_argv[1];
+    mpool_name = coll_info->tci_argv[1];
 
-    hse_kvdb_init();
+    rc = hse_kvdb_init();
+    if (hse_err_to_errno(rc))
+        return -1;
 
     return 0;
 }
@@ -39,88 +41,52 @@ MTF_BEGIN_UTEST_COLLECTION_PREPOST(kvdb_api_test, test_collection_setup, test_co
 
 MTF_DEFINE_UTEST(kvdb_api_test, kvdb_make_testcase)
 {
-    // TC: A KVDB with a valid name can be created on an existing MPOOL...
-
     hse_err_t rc;
 
-    rc = hse_kvdb_make(MPOOL_NAME, NULL);
-
-    if (hse_err_to_errno(rc) == EACCES) {
-        fprintf(stderr, "Invalid permissions");
-        exit(1);
-    }
-
-    ASSERT_EQ(hse_err_to_errno(rc), EXIT_SUCCESS);
+    // TC: A KVDB with a valid name can be created on an existing MPOOL
+    rc = hse_kvdb_make(mpool_name, NULL);
+    ASSERT_NE(hse_err_to_errno(rc), EACCES);
+    ASSERT_EQ(hse_err_to_errno(rc), EEXIST);
 }
 
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_open_testcase_no_mpool)
+MTF_DEFINE_UTEST(kvdb_api_test, kvdb_make_no_mpool_testcase)
 {
-    // TC: A non-existing KVDB cannot be opened...
-
     hse_err_t rc;
 
-    rc = hse_kvdb_open("fake_mpool", NULL, &KVDB_HANDLE);
-
+    // TC: A KVDB cannot be created on a non-existing MPOOL
+    rc = hse_kvdb_make("fake_mpool", NULL);
     ASSERT_EQ(hse_err_to_errno(rc), ENOENT);
 }
 
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_close_testcase_no_kvdb)
+MTF_DEFINE_UTEST(kvdb_api_test, kvdb_handle_no_mpool_testcase)
 {
-    // TC: A non-existing KVDB cannot be closed...
+    struct hse_kvdb *kvdb_handle = NULL;
+    hse_err_t        rc;
 
-    hse_err_t rc;
+    // TC: A non-existing KVDB cannot be opened
+    rc = hse_kvdb_open("fake_mpool", NULL, &kvdb_handle);
+    ASSERT_EQ(hse_err_to_errno(rc), ENOENT);
 
-    rc = hse_kvdb_close(KVDB_HANDLE);
-
+    // TC: A non-existing KVDB cannot be closed
+    rc = hse_kvdb_close(kvdb_handle);
     ASSERT_EQ(hse_err_to_errno(rc), EINVAL);
 }
 
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_make_testcase_no_mpool)
+MTF_DEFINE_UTEST(kvdb_api_test, kvdb_valid_testcase)
 {
-    // TC: A KVDB cannot be created on a non-existing MPOOL...
+    struct hse_kvdb *kvdb_handle;
+    hse_err_t        rc;
 
-    hse_err_t rc;
-
-    rc = hse_kvdb_make("fake_mpool", NULL);
-
-    ASSERT_EQ(hse_err_to_errno(rc), ENOENT);
-}
-
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_close_testcase)
-{
-    // TC: An existing KVDB which is open can be closed...
-
-    hse_err_t rc;
-
-    rc = hse_kvdb_open(MPOOL_NAME, NULL, &KVDB_HANDLE);
-    if (hse_err_to_errno(rc)) {
-        char buf[128];
-        hse_err_to_string(rc, buf, sizeof(buf), NULL);
-        fprintf(stderr, "kvdb_close_testcase: %s", buf);
-    }
-
-    rc = hse_kvdb_close(KVDB_HANDLE);
-    KVDB_HANDLE = NULL;
-
+    // TC: An existing KVDB can be opened
+    rc = hse_kvdb_open(mpool_name, NULL, &kvdb_handle);
     ASSERT_EQ(hse_err_to_errno(rc), EXIT_SUCCESS);
-}
 
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_open_testcase)
-{
-    // TC: An existing KVDB can be opened...
+    // TC: An opened KVDB return a valid handle
+    ASSERT_NE(kvdb_handle, NULL);
 
-    hse_err_t rc;
-
-    rc = hse_kvdb_open(MPOOL_NAME, NULL, &KVDB_HANDLE);
-
+    // TC: An opened KVDB can be closed
+    rc = hse_kvdb_close(kvdb_handle);
     ASSERT_EQ(hse_err_to_errno(rc), EXIT_SUCCESS);
-}
-
-MTF_DEFINE_UTEST(kvdb_api_test, kvdb_valid_handle_testcase)
-{
-    // TC: An opened KVDB returns a valid handle...
-
-    ASSERT_NE(KVDB_HANDLE, NULL);
 }
 
 MTF_END_UTEST_COLLECTION(kvdb_api_test)
