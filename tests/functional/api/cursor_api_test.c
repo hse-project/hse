a7a384c400aae89ebe11b7667146f8f8bc55194c
LEROY JACOB VARGIS <ljacobvargis@micron.com>
2021-03-29 12:50:03 -0500

    clean code, follow code standards

--- tests/functional/api/cursor_api_test.c
+++ tests/functional/api/cursor_api_test.c
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (C) 2015-2021 Micron Technology, Inc.  All rights reserved.
+ * Copyright (C) 2021 Micron Technology, Inc.  All rights reserved.
  */
 
 #include <hse_ut/framework.h>
@@ -29,24 +29,23 @@ test_collection_setup(struct mtf_test_info *info)
 
     rc = hse_kvdb_open(mpool_name, NULL, &kvdb_handle);
     if (rc)
-        return -1;
+        return rc;
 
     err = hse_kvdb_kvs_make(kvdb_handle, kvs_name, NULL);
-    if (!(hse_err_to_errno(err) == EXIT_SUCCESS || hse_err_to_errno(err) == EEXIST))
-        return -1;
+    if (!(hse_err_to_errno(err) == 0 || hse_err_to_errno(err) == EEXIST))
+        return hse_err_to_errno(err);
 
     rc = hse_kvdb_kvs_open(kvdb_handle, kvs_name, NULL, &kvs_handle);
     if (rc)
-        return -1;
+        return rc;
 
     for (int i = 0; i < key_value_pairs; i++) {
         snprintf(key, sizeof(key), "test_key_%02d", i);
         snprintf(val, sizeof(val), "test_value_%02d", i);
 
         rc = hse_kvs_put(kvs_handle, NULL, key, strlen(key), val, strlen(val));
-
         if (rc)
-            return -1;
+            return rc;
     }
 
     return EXIT_SUCCESS;
@@ -61,14 +60,13 @@ test_collection_teardown(struct mtf_test_info *info)
     for (int i = 0; i < key_value_pairs; i++) {
         snprintf(key, sizeof(key), "test_key_%02d", i);
         rc = hse_kvs_delete(kvs_handle, NULL, key, strlen(key));
-
         if (rc)
-            return -1;
+            return rc;
     }
 
     rc = hse_kvs_cursor_destroy(cursor);
     if (rc)
-        return -1;
+        return rc;
 
     return EXIT_SUCCESS;
 }
@@ -80,7 +78,7 @@ init_cursor(struct mtf_test_info *info)
 
     rc = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &cursor);
     if (rc)
-        return -1;
+        return rc;
 
     return EXIT_SUCCESS;
 }
@@ -93,14 +91,12 @@ MTF_BEGIN_UTEST_COLLECTION_PREPOST(
 MTF_DEFINE_UTEST(cursor_api_test, cursor_invalid_testcase)
 {
     hse_err_t              err;
-    struct hse_kvs *       kvs_handle = NULL;
-    struct hse_kvs_cursor *cursor = NULL;
     bool                   eof = false;
     const void *           cur_key, *cur_val;
     size_t                 cur_klen, cur_vlen;
 
     /* TC: A cursor cannot be created without a valid KVS */
-    err = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &cursor);
+    err = hse_kvs_cursor_create(NULL, NULL, NULL, 0, &cursor);
     ASSERT_EQ(hse_err_to_errno(err), EINVAL);
 
     /* TC: A null cursor cannot be used to read a KVS */
@@ -116,24 +112,24 @@ MTF_DEFINE_UTEST(cursor_api_test, cursor_valid_testcase)
 
     /* TC: A cursor can be created */
     err = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &cursor_handle);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     /* TC: A populated KVS returns a valid cursor */
     ASSERT_NE(cursor_handle, NULL);
 
     /* TC: A handle can be reused to create multiple cursors */
     err = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &duplicate_cursor_handle);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     /* TC: A cursor can be destroyed */
     err = hse_kvs_cursor_destroy(cursor_handle);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     err = hse_kvs_cursor_destroy(duplicate_cursor_handle);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 }
 
-MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_valid_read_testcase, init_cursor)
+MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_read_testcase, init_cursor)
 {
     hse_err_t   err;
     bool        eof = false;
@@ -143,10 +139,9 @@ MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_valid_read_testcase, init_cursor)
     char        read_buff[16], expec_buff[16];
 
     /* TC: A cursor can read key value pairs in a KVS, and returns the correct key value pairs */
-    eof = false;
     while (!eof) {
         err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-        ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+        ASSERT_EQ(hse_err_to_errno(err), 0);
 
         if (!eof) {
             snprintf(
@@ -174,11 +169,11 @@ MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_read_changes_testcase, init_cursor)
 
     /* TC: An existing cursor cannot see changes to the KVS */
     err = hse_kvs_put(kvs_handle, NULL, "extra_key", 9, "extra_value", 11);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     while (!eof) {
         err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-        ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+        ASSERT_EQ(hse_err_to_errno(err), 0);
 
         if (!eof)
             count++;
@@ -197,11 +192,11 @@ MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_seek_testcase, init_cursor)
     char *      search_key = "test_key_02";
 
     /* TC: A cursor can seek to a specific key */
-    err = hse_kvs_cursor_seek(cursor, NULL, search_key, 11, NULL, NULL);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    err = hse_kvs_cursor_seek(cursor, NULL, search_key, strlen(search_key), NULL, NULL);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     snprintf(read_buff, sizeof(read_buff), "%.*s", (int)cur_klen, (char *)cur_key);
     ASSERT_STREQ(search_key, read_buff);
@@ -209,10 +204,10 @@ MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_seek_testcase, init_cursor)
     /* TC: A cursor will be positioned at the first key if the specified key does not exist */
     eof = false;
     err = hse_kvs_cursor_seek(cursor, NULL, "fake_key", 8, NULL, NULL);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     snprintf(read_buff, sizeof(read_buff), "%.*s", (int)cur_klen, (char *)cur_key);
     ASSERT_STREQ("test_key_00", read_buff);
@@ -230,32 +225,32 @@ MTF_DEFINE_UTEST_PRE(cursor_api_test, cursor_multiple_testcase, init_cursor)
 
     /* TC: A KVS can have multiple cursors reading at different rates */
     err = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &duplicate_cursor);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     /* Fast Cursor */
     err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
     err = hse_kvs_cursor_read(cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
     sprintf(buff_1, "%.*s", (int)cur_klen, (char *)cur_key);
 
     /* Slow Cursor */
     err =
         hse_kvs_cursor_read(duplicate_cursor, NULL, &cur_key, &cur_klen, &cur_val, &cur_vlen, &eof);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
     sprintf(buff_2, "%.*s", (int)cur_klen, (char *)cur_key);
 
     ASSERT_STRNE(buff_1, buff_2);
 
     /* TC: A KVS with multiple cursors must update each cursor explicitly */
     err = hse_kvs_cursor_create(kvs_handle, NULL, NULL, 0, &duplicate_cursor);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     err = hse_kvs_put(kvs_handle, NULL, "extra_key", 9, "extra_value", 11);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     err = hse_kvs_cursor_update(cursor, NULL);
-    ASSERT_EQ(hse_err_to_errno(err), EXIT_SUCCESS);
+    ASSERT_EQ(hse_err_to_errno(err), 0);
 
     eof = false;
     while (!eof) {
