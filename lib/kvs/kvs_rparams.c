b1fad060664ab1d6ab7a602acf8722fbcc29c63c
Tristan Partin <tpartin@micron.com>
2021-06-29 10:58:39 -0500

    Fix defualt cursor ttl

--- lib/kvs/kvs_rparams.c
+++ lib/kvs/kvs_rparams.c
@@ -88,7 +88,7 @@ static const struct param_spec pspecs[] = {
         .ps_convert = param_default_converter,
         .ps_validate = param_default_validator,
         .ps_default_value = {
-            .as_uscalar = 0,
+            .as_uscalar = 1500,
         },
         .ps_bounds = {
             .as_uscalar = {
