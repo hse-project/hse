3bcee1ff159d9e3830f8060b3a46a581859e7db9
Tristan Partin <tpartin@micron.com>
2021-07-08 18:32:08 -0500

    Fix symbolic link on install

---
+++ scripts/meson-hooks/install/cli-symlink.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bindir=
+
+case $1 in
+  /*) bindir=$(sed -E 's/(.*?)\$MESON_INSTALL_PREFIX/\1/' <<< $MESON_INSTALL_DESTDIR_PREFIX)/$1 ;;
+  *) bindir=$MESON_INSTALL_DESTDIR_PREFIX/$1 ;;
+esac
+
+ln --symbolic --force "$bindir/$2" "$bindir/$3"
