0aefc4de929d3b3e82821b1234de9ffd5dc724aa
Greg Becker <gbecker@micron.com>
2021-10-08 11:57:29 -0500

    Fail gracefully if the endianess of omf and libhse do not match...

--- lib/mpool/src/mblock_fset.c
+++ lib/mpool/src/mblock_fset.c
@@ -74,10 +74,26 @@ mblock_metahdr_init(struct mblock_fset *mbfsp, struct mblock_metahdr *mh)
 static merr_t
 mblock_metahdr_validate(struct mblock_fset *mbfsp, struct mblock_metahdr *mh)
 {
+	if (mh->magic != MBLOCK_METAHDR_MAGIC) {
+		bool big = (HSE_OMF_BYTE_ORDER == __ORDER_BIG_ENDIAN__);
+
+		if (mh->magic != bswap_32(MBLOCK_METAHDR_MAGIC))
+			return merr(EBADMSG);
+
+		hse_log(HSE_ERR "%s: MDC format is %s endian, but libhse compiled to use %s endian,"
+				"try recompiling with -DHSE_OMF_BYTE_ORDER=__ORDER_%s_ENDIAN__",
+				__func__,
+				big ? "little" : "big",
+				big ? "big" : "little",
+				big ? "LITTLE" : "BIG");
+
+		return merr(EPROTO);
+	}
+
     if (mh->vers != MBLOCK_METAHDR_VERSION)
         return merr(EPROTO);
 
-    if ((mh->magic != MBLOCK_METAHDR_MAGIC) || (mh->mcid != mclass_id(mbfsp->mc)) ||
+    if ((mh->mcid != mclass_id(mbfsp->mc)) ||
         (mh->blkbits != MBID_BLOCK_BITS) || (mh->mcbits != MBID_MCID_BITS))
         return merr(EBADMSG);
 
