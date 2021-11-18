a1abbffbc78bcc37995eb291a87fdd8dd15f4513
Jacob M. Jacob <4000419+jjacob512@users.noreply.github.com>
2021-11-18 17:24:13 -0600

    Adds errno in error message. Fixes section boundary checks. Removes redundant code. Utils strerror prints files info.
    Signed-off-by: Jacob M. Jacob <4000419+jjacob512@users.noreply.github.com>

--- tests/unit/util/hse_err_test.c
+++ tests/unit/util/hse_err_test.c
@@ -7,6 +7,9 @@
 
 #include <hse_util/hse_err.h>
 
+extern uint8_t __start_hse_merr;
+extern uint8_t __stop_hse_merr;
+
 MTF_MODULE_UNDER_TEST(hse_platform);
 
 int
@@ -57,7 +60,7 @@ MTF_DEFINE_UTEST(hse_err_test, merr_test_1)
 
     (void)merr_strinfo(err, errinfo, sizeof(errinfo), 0);
     errmsg = strerror_r(merr_errno(err), errbuf, sizeof(errbuf));
-    ASSERT_EQ(0, strcmp(strstr(errinfo, "I"), errmsg));
+    ASSERT_EQ(0, strncmp(strstr(errinfo, "I"), errmsg, strlen(errmsg)));
 
     file = hse_merr_base;
     err = merr_pack(EAGAIN, 0, file, 456);
@@ -65,15 +68,20 @@ MTF_DEFINE_UTEST(hse_err_test, merr_test_1)
     ASSERT_EQ(456, merr_lineno(err));
     ASSERT_EQ(NULL, merr_file(err));
 
-    err = merr_pack(EBUG, 0, (char *)1, 123);
-    ASSERT_EQ(EBUG, merr_errno(err));
-    ASSERT_EQ(123, merr_lineno(err));
-    ASSERT_EQ(0, strcmp(merr_file(err), hse_merr_bug0));
-
-    file = hse_merr_base + sizeof(file);
+    /* testing an invalid file pointer out side the merr section */
+    file = (char*)&__stop_hse_merr;
     err = merr_pack(EAGAIN, 0, file, 456);
     ASSERT_EQ(EAGAIN, merr_errno(err));
     ASSERT_EQ(456, merr_lineno(err));
+    ASSERT_EQ(0, strcmp(merr_file(err), hse_merr_bug0));
+
+    /*
+     * testing a file pointer that is within merr the section
+     * that is not properly aligned
+     */
+    err = merr_pack(EBUG, 0, (char*)&__start_hse_merr + 1, 123);
+    ASSERT_EQ(EBUG, merr_errno(err));
+    ASSERT_EQ(123, merr_lineno(err));
     ASSERT_EQ(0, strcmp(merr_file(err), hse_merr_bug1));
 
     err = merr(EBUG);
