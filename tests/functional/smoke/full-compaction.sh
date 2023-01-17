59aaaf81effa9a9aabe3592c84f5b6a8b319fc9f
Alex Tomlinson <atomlinson@micron.com>
2023-01-17 09:19:58 -0600

    Tweak full-compact smoke test
    Signed-off-by: Alex Tomlinson <atomlinson@micron.com>

--- tests/functional/smoke/full-compaction.sh
+++ tests/functional/smoke/full-compaction.sh
@@ -16,6 +16,6 @@ kvs=$(kvs_create smoke-0)
 cmd kmt "$home" "$kvs" -b -i128m -l16 -s1 -t10 -j64
 cmd hse kvdb compact -f -t 1200 $home
 
-uncompacted_nodecnt=$(cn_metrics $home $kvs | grep "^n " | awk '{print $3}' | grep -v 1 || echo 0)
+cn_metrics "$home" "$kvs" | grep -q -P "^t\s+1\s+1\b"
+[[ $? == 0 ]] || err "Tree is not fully compacted"
 
-[[ $uncompacted_nodecnt == 0 ]] || err "Tree is not fully compacted:\n$tree"
