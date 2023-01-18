d476c8da3829e5b9ec11786d8baa745cf84705fa
Gaurav Ramdasi <10132364+gsramdasi@users.noreply.github.com>
2023-01-18 15:37:10 -0600

    Appease shellcheck | Other fixes
    Signed-off-by: Gaurav Ramdasi <10132364+gsramdasi@users.noreply.github.com>

--- tests/functional/smoke/full-compaction.sh
+++ tests/functional/smoke/full-compaction.sh
@@ -2,9 +2,9 @@
 
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2021-2022 Micron Technology, Inc. All rights reserved.
+# Copyright (C) 2021-2023 Micron Technology, Inc. All rights reserved.
 
-#doc: simple transaction test on a KVDB kvs
+#doc: Test the full compaction cli commandline
 
 . common.subr
 
@@ -14,8 +14,9 @@ kvdb_create
 kvs=$(kvs_create smoke-0)
 
 cmd kmt "$home" "$kvs" -b -i128m -l16 -s1 -t10 -j64
-cmd hse kvdb compact -f -t 1200 $home
+cmd hse kvdb compact -f -t 1200 "$home"
 
 cn_metrics "$home" "$kvs" | grep -q -P "^t\s+1\s+1\b"
-[[ $? == 0 ]] || err "Tree is not fully compacted"
+rc=$?
 
+[[ $rc == 0 ]] || err "Tree is not fully compacted"
